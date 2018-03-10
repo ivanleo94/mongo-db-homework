ce04b16fab5cb2d6e5ec8d8633ebea53aceb11ca
ivanleo94 <31164505+ivanleo94@users.noreply.github.com>
2018-03-10 13:31:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
